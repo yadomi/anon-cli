ecd1f40b5a1fe55205ec696a343e0e9648da62ba
Felix Yadomi <felix.yadomi@gmail.com>
2019-07-29 17:12:47 +0200

    Rewrite logic to detect and parse COPY statement to get table and column names

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const argv = require("minimist")(process.argv.slice(2));
-const md5 = require("md5");
-const R = require("ramda");
 const { resolve, basename } = require("path");
 const {
   existsSync: exists,
@@ -11,28 +9,28 @@ const Stream = require("stream");
 const Readline = require("readline");
 
 const bin = basename(process.argv[1]);
+const configPath = argv["c"];
 const input = argv["i"];
 const output = argv["o"];
 const force = argv["f"];
 
-const anonymize = R.compose(
-  R.join(""),
-  R.append(".com"),
-  R.insert("12", "@"),
-  md5
-);
-
 function usage() {
-  console.log(`usage: ${bin} -i input.sql -o output.sql [-f]`);
+  console.log(`usage: ${bin} -c ./config.js -i input.sql -o output.sql [-f]`);
 }
 
-if (!input || !output) {
+if (!input || !output || !configPath) {
   usage();
   process.exit(2);
 }
 
 const inputfile = resolve(input);
 const outputfile = resolve(output);
+const configFile = resolve(configPath);
+
+if (!exists(configFile)) {
+  console.log(`${bin}: no such file or directory: ${configPath}`);
+  process.exit(2);
+}
 
 if (!exists(inputfile)) {
   console.log(`${bin}: no such file or directory: ${input}`);
@@ -44,18 +42,59 @@ if (exists(outputfile) && !force) {
   process.exit(2);
 }
 
-const EMAIL_REGEX = /([a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*)/;
-
+const config = require(configFile);
 const instream = createReadStream(inputfile);
 const outstream = createWriteStream(outputfile);
 
 const lines = Readline.createInterface(instream, outstream);
 
-const replacer = match => {
-  return anonymize(match);
+const RE = {
+  TABLE: /COPY\spublic\."?([a-z_]*)"?\s\(/,
+  COLUMNS: /COPY public.*\((.*)\)/
 };
 
+let currentTable = null;
+let currentColumns = [];
+
 lines.on("line", line => {
-  const altered = line.replace(EMAIL_REGEX, replacer);
-  outstream.write(altered + "\n");
+  /**
+   * Reset the currentTable at the end of the insert/copy statement
+   */
+  if (line === "\\.") {
+    currentColumns = [];
+    currentTable = null;
+  }
+
+  /**
+   * Replace/Redact data in specfied column from config
+   */
+  if (currentTable) {
+    const transforms = config.tables[currentTable];
+    const data = line.split("\t");
+    for (const column of currentColumns) {
+      if (column in transforms) {
+        const from = data[currentColumns.indexOf(column)];
+        const to = transforms[column](from);
+        if (!from || from === `\\N`) continue;
+        line = line.replace(from, to);
+      }
+    }
+  }
+
+  /**
+   * Match data insertion start
+   */
+  if (/COPY public/.test(line)) {
+    const tableName = line.match(RE.TABLE)[1];
+
+    if (tableName in config.tables) {
+      currentTable = tableName;
+
+      const raw = line.match(RE.COLUMNS)[1];
+      const columns = raw.replace(/['"]+/g, "").split(", ");
+      currentColumns = columns;
+    }
+  }
+
+  outstream.write(line + "\n");
 });
